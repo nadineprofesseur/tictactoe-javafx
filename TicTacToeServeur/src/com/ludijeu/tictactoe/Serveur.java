94ed687902a266adf2e61d7813ad82eafc168ff6
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-21 21:40:54 -0500

    Ouvrir le port

--- TicTacToeServeur/src/com/ludijeu/tictactoe/Serveur.java
+++ TicTacToeServeur/src/com/ludijeu/tictactoe/Serveur.java
@@ -1,10 +1,31 @@
 package com.ludijeu.tictactoe;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Serveur {
 	
+	public static int PORT = 2000;
+	private ServerSocket ecouteur = null;
+
 	public Serveur()
 	{
-		
+		this.ouvrir();
 	}
+	
+	public boolean ouvrir()
+	{
+				
+		try {
+			this.ecouteur = new ServerSocket(PORT);
+			System.out.println("Lancement de l'ecoute du serveur");			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
 
 }
